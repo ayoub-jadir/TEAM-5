f335a47ef14ed785515f66a283617283a7eeb6d7
Valentina <lilithsixx@gmail.com>
2022-01-23 17:58:32 +0100

    feat:starting page Exchange adding Chart feature

--- js/script-valentina.js
+++ js/script-valentina.js
@@ -7,7 +7,7 @@ function mostraLista() {
 
     let ajax = new XMLHttpRequest();
 
-    ajax.open("GET", "langs.json", true);
+    ajax.open("GET", url, true);
 
     ajax.onload = function () {
         if (this.status == 200) {
@@ -39,9 +39,48 @@ function mostraLista() {
 // Script in JQuery per animazione Menu Dropdown in Hover;
 $(document).ready(function () {
     $(".dropdown").hover(function () {
-        var dropdownMenu = $(this).children(".dropdown-menu");
+        let dropdownMenu = $(this).children(".dropdown-menu");
         if (dropdownMenu.is(":visible")) {
             dropdownMenu.parent().toggleClass("open");
         }
     });
-});     
+});
+
+
+// Grafico pagina Exchange;
+const ctx = document.getElementById('myChart');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            fill: true,
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
